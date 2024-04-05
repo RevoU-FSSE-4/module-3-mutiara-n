e245b56a18e6cae5d39f782093b9694c49f6a96a
mutiara-n <mutiara.nafysah11@gmail.com>
2024-04-05 13:10:51 +0700

    new js file

--- app.js
+++ app.js
@@ -1,76 +1,120 @@
-// Place your code here
-// Add any additional code necessary to fulfill the requirements of the assignment
-
-document.addEventListener('DOMContentLoaded', function() {
-    const taskList = document.getElementById('taskList');
-    const newTaskInput = document.getElementById('newTask');
-    const addTaskBtn = document.getElementById('addTaskBtn');
-
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+document.addEventListener('DOMContentLoaded', function () {
+    var taskList = document.getElementById('taskList');
+    var newTaskInput = document.getElementById('newTask');
+    var addTaskBtn = document.getElementById('addTaskBtn');
     // Fetch tasks from API
-    async function fetchTasks() {
-        try {
-            const response = await fetch("https://module3-api-is2m.onrender.com/random-todos");
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error("Error Fetching Tasks", error);
-        }
-    }
-
-    // Validation Form
-    addTaskBtn.addEventListener('click', function() {
-        const taskTitle = newTaskInput.value.trim();
-    
-        if (taskTitle === '') {
-          alert('Task cannot be blank.');
-          return;
-        }
-    
-        addTask(taskTitle);
-        newTaskInput.value = '';
-    });    
-        
-    // Add Task Button Function
-    function addTask(taskTitle) {
-        const li = document.createElement('li');
-        const taskItem = document.createTextNode(taskTitle);
-        li.appendChild(taskItem);
-        
-        const completeButton = document.createElement('button');
-        completeButton.innerText = 'Complete';
-        completeButton.addEventListener('click', function() {
-        completeTask(li);
+    var fetchTasks = function () { return __awaiter(_this, void 0, void 0, function () {
+        var api, response, data, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    api = 'https://module3-api-is2m.onrender.com/random-todos';
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 4, , 5]);
+                    return [4 /*yield*/, fetch(api)];
+                case 2:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 3:
+                    data = _a.sent();
+                    return [2 /*return*/, data];
+                case 4:
+                    error_1 = _a.sent();
+                    console.error("Error fetcheing task:", error_1);
+                    throw (error_1);
+                case 5: return [2 /*return*/];
+            }
         });
-
-        const removeButton = document.createElement('button');
-        removeButton.innerText = 'Remove';
-        removeButton.addEventListener('click', function() {
-        removeTask(li);
-        });        
-    
-        li.appendChild(completeButton);
-        li.appendChild(removeButton);
-  
-        taskList.appendChild(li);
+    }); };
+    // Displaying Tasks
+    function displayTask(tasks) {
+        tasks.forEach(function (task) {
+            var taskItem = createTask(task);
+            taskList.appendChild(taskItem);
+        });
     }
-  
+    // Add task button function
+    function createTask(task) {
+        // Create Task
+        var taskItem = document.createElement('li');
+        taskItem.textContent = task;
+        taskItem.classList.add("task-item");
+        // Complete Button
+        var completeButton = document.createElement('button');
+        completeButton.innerText = 'Complete';
+        completeButton.addEventListener('click', function () {
+            completeTask(taskItem);
+        });
+        taskItem.appendChild(completeButton);
+        // Remove Button
+        var removeButton = document.createElement('button');
+        removeButton.innerText = 'Remove';
+        removeButton.addEventListener('click', function () {
+            removeTask(taskItem);
+        });
+        taskItem.appendChild(removeButton);
+        return taskItem;
+    }
+    function addTask() {
+        var taskTitle = newTaskInput.value.trim();
+        if (taskTitle === '') {
+            alert('Task cannot be blank');
+            return;
+        }
+        var taskItem = createTask(taskTitle);
+        taskList.appendChild(taskItem);
+        newTaskInput.value = '';
+    }
+    addTaskBtn.addEventListener("click", addTask);
     function completeTask(taskElement) {
         taskElement.style.textDecoration = 'line-through';
+        taskElement.style.backgroundColor = '#ddd';
         taskElement.querySelector('button').disabled = true;
     }
-  
     function removeTask(taskId) {
         taskList.removeChild(taskId);
     }
-
-    // Initially display 3 random tasks from API
+    // Fetch tasks from API
     fetchTasks()
-    .then (tasks => {
-        tasks.forEach(task => {
-            const li = `<li>${task}</li>`;
-            document.querySelector('ul').insertAdjacentHTML('beforeend', li);
-        });
-    })
-    .then(tasks => tasks.slice(0, 3))
-    .then(displayTasks)
+        .then(function (tasks) { return tasks.slice(0, 3); })
+        .then(displayTask)
+        .catch(function (error) { return console.error("Error fetching and displaying task:", error); });
 });
